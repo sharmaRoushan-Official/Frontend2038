fdc8ffbc7b15cd4efde7ce67143b28ebd6e55fa8
Aditya Pimpale <pimpaleaditya2@gmail.com>
2026-07-09 17:40:59 +0530

    feat(javascript): add basic number addition script

---
+++ Aditya's_Assignments/JavaScript/script.js
@@ -0,0 +1,8 @@
+console.log("Welcome to JS")
+
+
+const prompt = require("prompt-sync")();
+
+let a  = Number(prompt("Enter the first NUmber: - "))
+let b  = Number(prompt("Enter the second NUmber: - "))
+console.log(a+b)
