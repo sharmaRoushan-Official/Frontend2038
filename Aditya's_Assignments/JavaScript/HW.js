b585bb210683ab63e7e0e3a73f3da940e8deed42
Aditya Pimpale <pimpaleaditya2@gmail.com>
2026-07-16 13:31:43 +0530

    feat(javascript): add new homework file and update script with arithmetic operations

---
+++ Aditya's_Assignments/JavaScript/HW.js
@@ -0,0 +1,35 @@
+// Home work 
+
+// ----
+ 
+
+// 3. Wap to take input length and breadth of rectagle and find it's area, perimeter and diagonal.
+// 4. Wap to take input radius of circle and find the area and circumference of the circle.
+// 5. wap to create temperature calculatr-
+//     a). °F to °C 
+//     b). °C to °F
+// 6. wap to take input five number and find their sum and average.
+
+const prompt = require("prompt-sync")();
+// 1. wap to take tow input number from user and swap using third variables.
+
+// console.log(`before swap numbers are a is now ${a} and b is now ${b}`)
+// var c;
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log(`swapped numbers are a is now ${a} and b is now ${b}`)
+
+
+// 2. wap to take two inoput and swap without using thrid variabe.
+
+// var a  = parseInt(prompt("Enter number 1: "));
+// var b  = parseInt(prompt("Enter number 2: "));
+// console.log(`before swap numbers are a is now ${a} and b is now ${b}`)
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(`swapped numbers are a is now ${a} and b is now ${b}`)
+
--- Aditya's_Assignments/JavaScript/script.js
+++ Aditya's_Assignments/JavaScript/script.js
@@ -1,8 +1,25 @@
-console.log("Welcome to JS")
-
-
 const prompt = require("prompt-sync")();
 
-let a  = Number(prompt("Enter the first NUmber: - "))
-let b  = Number(prompt("Enter the second NUmber: - "))
-console.log(a+b)
+var a = parseInt(prompt("Enter 1st number: "));
+
+var b = parseInt(prompt("Enter Second number: "));
+
+var c = a + b;
+
+var d = a / b;
+
+var e = a * b;
+
+var f = a - b;
+
+var g = a % b;
+
+console.log(`the sum of ${a} and ${b} is ${c}`);
+
+console.log(`the division of ${a} by ${b} is ${d}`);
+
+console.log(`the multiplication of ${a} with ${b} is ${e}`)
+
+console.log(`the subtraction of ${a} from ${b} is ${f}`)
+
+console.log(`the modulo of ${a} and ${b} is ${g}`)
