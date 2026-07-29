183ad8c295a3ef5d0775709ef19407849ebf1f65
sharmaRoushan-Official <roushankr26@gmail.com>
2026-07-29 17:23:28 +0530

    feat(javaScript): add array manipulation examples and methods

---
+++ JavaScript/13.Array/2_array.js
@@ -0,0 +1,19 @@
+// Method 1 : Array Literal 
+
+let fruits = ["Apple","Mango","Banana"];
+
+console.log(fruits);
+
+// Method 2: new Array() 
+
+let numbers = new Array(10,20,30);
+console.log(numbers);
+
+// Method 3: Empty Array 
+
+let students = [];
+console.log(students);
+
+
+// Accessing Elements 
+
---
+++ JavaScript/13.Array/3_array.js
@@ -0,0 +1,21 @@
+let fruits  = ["Apple","Mango","Banana"];
+console.log(fruits);
+
+
+// Accessing Elements 
+console.log(fruits[0]); // Apple
+console.log(fruits[1]); // Mango
+console.log(fruits[2]); // Banana
+
+// Updating Elements 
+
+fruits[1] = "Orange"; // Mango = Orange
+console.log(fruits);
+
+
+// Array Length
+
+console.log(fruits.length);
+
+
+// 2. Array Methods
